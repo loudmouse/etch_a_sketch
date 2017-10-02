38c2fe9ab9785ebd25a7c21d3ed4f1c2a98f10d2
Nolan Neeley <nolan@artmill.com>
2017-10-02 14:40:47 -0500

    added a function that generates a div within a div. Also added a for statement that appends a specified number of divs to the original div. Next I added two events to change the background color of the divs as you enter and leave the divs with your mouse. I still need to figure out how to split the dynamically generated grids into rows. Also need to figure out how to add a tracer to the path of the mouse as it enters and leaves the divs.

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-window.onload = function() {
-  var $div = $("<div>", {id: "foo", "class": "a"});
+/*
+
+I used this jQuery resource for dynamically creating a div with an ID and Class.
+From: https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
+
+var $div = $("<div>", {id: "foo", "class": "a"});
+$div.click(function(){  });
+$("#box").append($div);
+
+
+I'm using this to create a for loop to dynamically generate a specific number of these divs.
+https://stackoverflow.com/questions/16262462/generate-a-set-number-of-divs-with-a-certain-class
+
+Use a for loop:
+
+$(document).ready(function() {
+  var nrOfDivs = 12 // for example
+  for (i=0;i<nrOfDivs;i++) {
+    $('#divToAppendTo').append('<div class="music_visualizer_bar"></div>');
+  };
+
+});
+Just change the nrOfDivs to the amount of divs you need :)
+
+*/
+
+$(document).ready(function() {
+  var $div = $("<div>", {"class": "divGrids"});
   $div.click(function(){ /* ... */ });
   $("#main-container").append($div);
-}
 
-  /*
-  var $div = $("<div>", {id: "foo", "class": "a"});
-  $div.click(function(){  });
-  $("#box").append($div);
-  */
+var numberOfDivs = 15
+for (i=0;i<numberOfDivs;i++) {
+  $('#main-container').append('<div class="divGrids"></div>');
+};
+
+$('#main-container').on('mouseenter', '.divGrids', function() {
+  $(this).addClass('hoverOn');
+});
+
+$('#main-container').on('mouseleave', '.divGrids', function() {
+  $(this).removeClass('hoverOn');
+});
+
+
+
+
+});
